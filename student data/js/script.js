3d267be7a87addb0102785224f604c893b4ecb28
Ragini Rakholiya <145538394+rakholiyaragini@users.noreply.github.com>
2024-06-03 11:23:50 +0530

    Add files via upload

---
+++ student data/js/script.js
@@ -0,0 +1,220 @@
+let students = [
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Priya Patel",
+    mail : "priyupatel0987@gmail.com",
+    Age : 20,
+    city : "Surat, Gujrat, India.",
+    contact : 8897654335,
+    hobby : ["Dancing , Eating"],
+},
+{
+    name : "Ragini Rakholiya",
+    mail : "raginirakholiya123@gmail.com",
+    Age : 17,
+    city : "Surat, Gujrat, India.",
+    contact : 8980150444,
+    hobby : ["Shopping , Reading"],
+},
+{
+    name : "Yashika Rakholiya",
+    mail : "yashikarakholiya123@gmail.com",
+    Age : 21,
+    city : "Surat, Gujrat, India.",
+    contact : 76738362784,
+    hobby : ["Shopping , Reading"],
+},
+]
+for(let i=0; i<students.length; i++){
+
+    main.innerHTML += `<div class="col-md-3"><div class="card rounded-2xl border-white" style="width: 280px; height: 330px; background-color: #2C3045; margin-top: 100px;">
+    <img src= "image/user.jpg" alt="img" style = "width : 130px; height : 130px; border-radius : 50%; margin-top : -70px; " class = "mx-auto">
+    <h2 class="text-center mt-4 text-white fw-normal" style="font-size : 17px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top : -3px" width="18" height="18" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+    </svg> : ${students[i].name}</h2>
+    <h2 class="text-center mt-2 text-white fw-normal" style="font-size : 14px"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+    </svg> : ${students[i].mail}</h2>
+    <h2 class="text-center mt-2 text-white fw-normal" style="font-size : 14px"><svg xmlns="http://www.w3.org/2000/svg"style="margin-top : -3px" width="14" height="14" fill="currentColor" class="bi bi-cake2" viewBox="0 0 16 16">
+    <path d="m3.494.013-.595.79A.747.747 0 0 0 3 1.814v2.683q-.224.051-.432.107c-.702.187-1.305.418-1.745.696C.408 5.56 0 5.954 0 6.5v7c0 .546.408.94.823 1.201.44.278 1.043.51 1.745.696C3.978 15.773 5.898 16 8 16s4.022-.227 5.432-.603c.701-.187 1.305-.418 1.745-.696.415-.261.823-.655.823-1.201v-7c0-.546-.408-.94-.823-1.201-.44-.278-1.043-.51-1.745-.696A12 12 0 0 0 13 4.496v-2.69a.747.747 0 0 0 .092-1.004l-.598-.79-.595.792A.747.747 0 0 0 12 1.813V4.3a22 22 0 0 0-2-.23V1.806a.747.747 0 0 0 .092-1.004l-.598-.79-.595.792A.747.747 0 0 0 9 1.813v2.204a29 29 0 0 0-2 0V1.806A.747.747 0 0 0 7.092.802l-.598-.79-.595.792A.747.747 0 0 0 6 1.813V4.07c-.71.05-1.383.129-2 .23V1.806A.747.747 0 0 0 4.092.802zm-.668 5.556L3 5.524v.967q.468.111 1 .201V5.315a21 21 0 0 1 2-.242v1.855q.488.036 1 .054V5.018a28 28 0 0 1 2 0v1.964q.512-.018 1-.054V5.073c.72.054 1.393.137 2 .242v1.377q.532-.09 1-.201v-.967l.175.045c.655.175 1.15.374 1.469.575.344.217.356.35.356.356s-.012.139-.356.356c-.319.2-.814.4-1.47.575C11.87 7.78 10.041 8 8 8c-2.04 0-3.87-.221-5.174-.569-.656-.175-1.151-.374-1.47-.575C1.012 6.639 1 6.506 1 6.5s.012-.139.356-.356c.319-.2.814-.4 1.47-.575M15 7.806v1.027l-.68.907a.94.94 0 0 1-1.17.276 1.94 1.94 0 0 0-2.236.363l-.348.348a1 1 0 0 1-1.307.092l-.06-.044a2 2 0 0 0-2.399 0l-.06.044a1 1 0 0 1-1.306-.092l-.35-.35a1.935 1.935 0 0 0-2.233-.362.935.935 0 0 1-1.168-.277L1 8.82V7.806c.42.232.956.428 1.568.591C3.978 8.773 5.898 9 8 9s4.022-.227 5.432-.603c.612-.163 1.149-.36 1.568-.591m0 2.679V13.5c0 .006-.012.139-.356.355-.319.202-.814.401-1.47.576C11.87 14.78 10.041 15 8 15c-2.04 0-3.87-.221-5.174-.569-.656-.175-1.151-.374-1.47-.575-.344-.217-.356-.35-.356-.356v-3.02a1.935 1.935 0 0 0 2.298.43.935.935 0 0 1 1.08.175l.348.349a2 2 0 0 0 2.615.185l.059-.044a1 1 0 0 1 1.2 0l.06.044a2 2 0 0 0 2.613-.185l.348-.348a.94.94 0 0 1 1.082-.175c.781.39 1.718.208 2.297-.426"/>
+    </svg> : ${students[i].Age}</h2>
+    <h2 class="text-center mt-2 text-white fw-normal" style="font-size : 14px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top : -3px" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+    <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+    </svg> : ${students[i].contact}</h2>
+    <h2 class="text-center mt-2 text-white fw-normal" style="font-size : 14px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top : -3px" width="16" height="16" fill="currentColor" class="bi bi-suit-heart" viewBox="0 0 16 16">
+    <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.6 7.6 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
+    </svg> : ${students[i].hobby}</h2>
+    <div style="border-bottom: 1px solid gray; width: 230px; margin-left : 25px" class="opacity-75 mt-3"></div>
+    <h2 class="text-center mt-3 text-white fw-normal" style="font-size : 14px"><svg xmlns="http://www.w3.org/2000/svg" style="margin-top : -3px" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+    </svg> : ${students[i].city}</h2>
+    </div></div>`
+}
